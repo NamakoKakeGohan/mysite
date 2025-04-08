83f480e67e4bdd4788f93219f7940c6990b94feb
namako1215 <closure1215@0utlook.com>
2025-04-09 02:45:23 +0900

    main.jsにfirebaseを使用するコードの実装

--- game-introduction/src/main.js
+++ game-introduction/src/main.js
@@ -1,5 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { firestorePlugin } from "vuefire";
+import { firebaseDb } from "./firebase/firebaseInit"; // Firebaseの初期化
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+
+app.use(router);
+app.use(firestorePlugin); // VueFire プラグインを登録
+app.mount("#app");

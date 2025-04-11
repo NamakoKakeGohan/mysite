72928e27cf37f85a32fa8671f539d7ec22aa5b2a
namako1215 <closure1215@0utlook.com>
2025-04-11 16:22:17 +0900

    Revert "共通で使用するコンポーネントを作成" とのコンクリフト発生の為、次のコミットを打ち消し "不要なコミットの打ち消し "大枠の作成""
    This reverts commit 6b998260816faae51f02f5b76914ed7d2104cf8b.

--- game-introduction/src/App.vue
+++ game-introduction/src/App.vue
@@ -1,30 +1,59 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="layout">
+    <ProjectHeader class="header" />
+    <HambergerMenu />
+    <ProjectSidebar class="sidebar" />
+    <main>
+      <router-view />
+    </main>
+    <ProjectFooter />
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script setup>
+import ProjectHeader from "./components/ProjectHeader.vue";
+import HambergerMenu from "./components/HambergerMenu.vue";
+import ProjectSidebar from "./components/ProjectSidebar.vue";
+import ProjectFooter from "./components/ProjectFooter.vue";
+</script>
+
+<style scoped>
+.layout {
+  display: grid;
+  grid-template-areas:
+    "header header"
+    "sidebar main"
+    "footer footer";
+  grid-template-columns: 200px 1fr;
+  grid-template-rows: auto 1fr auto;
+  height: 100vh;
 }
 
-nav {
-  padding: 30px;
+.header {
+  grid-area: header;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 30;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.sidebar {
+  grid-area: sidebar;
+  position: fixed;
+  top: 100px;
+  left: 0;
+  height: calc(100vh - 60px);
+  z-index: 20;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+main {
+  grid-area: main;
+  padding: 1rem;
+  margin-top: 100px;
+}
+
+footer {
+  grid-area: footer;
 }
 </style>
---
+++ game-introduction/src/components/ProjectFooter.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer>
+    <p>(c) 2019 SteamGridDB</p>
+  </footer>
+</template>
+
+<script setup></script>
+
+<style scoped>
+footer {
+  max-width: 100%;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 100px;
+  background-color: #265e9e;
+  color: white;
+}
+
+h1 {
+  font-size: 4rem;
+}
+</style>

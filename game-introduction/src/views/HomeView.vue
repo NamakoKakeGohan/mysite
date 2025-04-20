7992bd43aebafb7709e1c41644017864e0677061
namako1215 <closure1215@0utlook.com>
2025-04-20 21:47:46 +0900

    HomeViewのコードを見やすいように修正

--- game-introduction/src/views/HomeView.vue
+++ game-introduction/src/views/HomeView.vue
@@ -1,37 +1,19 @@
 <template>
   <div class="home">
-    <PostItem
-      v-if="post"
-      :post="post"
-    />
-    <p v-else>
-      投稿を読み込んでいます...
-    </p>
+    <PostItem v-if="posts" />
+    <p v-else>Recsを読み込み中...</p>
   </div>
   
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
-import { firebaseDb } from "../firebase/firebaseInit";
-import { doc, getDoc } from "firebase/firestore";
+import { ref } from "vue";
 import PostItem from "../components/PostItem.vue";
 
-const post = ref(null);
+const posts = ref(null);
 
-// Firestore から特定の投稿を取得
-async function fetchPost() {
-  const docRef = doc(firebaseDb, "post", "7zSlaxhvcS3PmcTlScmd"); // ドキュメントIDを指定
-  const docSnap = await getDoc(docRef);
+// 投稿を取得
 
-  if (docSnap.exists()) {
-    post.value = { id: docSnap.id, ...docSnap.data() }; // データを取得して格納
-  } else {
-    console.error("指定された投稿が見つかりませんでした");
-  }
-}
-
-onMounted(fetchPost);
 </script>
 
 <style scoped>

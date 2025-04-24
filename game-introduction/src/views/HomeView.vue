a94e294db1378b35be6b5fd021d0476ec12a9fcc
namako1215 <closure1215@0utlook.com>
2025-04-24 18:14:20 +0900

    postData.jsをHomeViewに直接代入

--- game-introduction/src/views/HomeView.vue
+++ game-introduction/src/views/HomeView.vue
@@ -1,24 +1,30 @@
 <template>
   <div class="home">
-    <PostItem v-if="posts" />
+    <!-- recsList を PostItem に渡す -->
+    <PostItem v-if="recsList" />
     <p v-else>Recsを読み込み中...</p>
   </div>
-  
 </template>
 
 <script setup>
 import { ref } from "vue";
 import PostItem from "../components/PostItem.vue";
+import postData from "../postData"; // 投稿データをインポート
 
-const posts = ref(null);
+// 投稿データを格納する変数
+const recsList = ref(null);
 
 // 投稿を取得
-
+recsList.value = postData; // postData を直接代入
 </script>
 
 <style scoped>
 .home {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-right: 90px;
   padding: 20px;
-  background-color: #f5f8fa;
 }
+
 </style>

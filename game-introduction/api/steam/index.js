32d17390cc6685ebfc11b0d423baa0cd9bad1d54
namako1215 <closure1215@0utlook.com>
2025-06-22 17:34:38 +0900

    APIハンドラーのアプリ一覧取得処理を修正し、API_KEYのパラメータを削除

--- game-introduction/api/steam/index.js
+++ game-introduction/api/steam/index.js
@@ -19,18 +19,14 @@ export default async function handler(req, res) {
   const { appid, type } = req.query;
 
   if (type === "apps") {
-    // アプリ一覧取得
     try {
-      const response = await axios.get(`${BASE_URL}/ISteamApps/GetAppList/v2/`, {
-        params: { key: API_KEY },
-      });
+      const response = await axios.get(`${BASE_URL}/ISteamApps/GetAppList/v2/`);
       res.status(200).json(response.data);
     } catch (error) {
       console.error("アプリ一覧取得中にエラー:", error.message);
       res.status(500).json({ error: error.message });
     }
   } else if (type === "appdetails" && appid) {
-    // アプリ詳細取得
     try {
       const response = await axios.get(
         `https://store.steampowered.com/api/appdetails?appids=${appid}&cc=jp&l=japanese`

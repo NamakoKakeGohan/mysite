120d94c852f611200079e4c6add2b04d4dda1908
namako1215 <closure1215@0utlook.com>
2025-06-28 22:16:49 +0900

    フッターにGitHubとメールアイコンを追加し、著作権表示を削除

--- game-introduction/src/components/ProjectFooter.vue
+++ game-introduction/src/components/ProjectFooter.vue
@@ -1,10 +1,18 @@
 <template>
   <footer>
-    <p>(c) 2019 SteamGridDB</p>
+    <a href="https://github.com/NamakoKakeGohan/mysite">
+    <img :src="GitHubIcon" alt="GitHubアイコン" />
+    </a>
+    <a href="mailto:closure1215@outlook.com?subject=件名を入力&body=本文を入力">
+    <img :src="MailIcon" alt="メールアイコン" />
+    </a>
   </footer>
 </template>
 
-<script setup></script>
+<script setup>
+import GitHubIcon from "../assets/GitHub.png";
+import MailIcon from "../assets/MAIL.png";
+</script>
 
 <style scoped>
 footer {
@@ -21,4 +29,22 @@ footer {
 h1 {
   font-size: 4rem;
 }
+
+footer img {
+  width: 36px;
+  height: 36px;
+  margin: 0 16px;
+  object-fit: contain;
+  transition: transform 0.2s;
+  filter: brightness(0) invert(1); /* 白色アイコンに統一 */
+  background: transparent;
+  border-radius: 8px;
+  display: inline-block;
+  vertical-align: middle;
+}
+footer img:hover {
+  transform: scale(1.15);
+}
+
+
 </style>
